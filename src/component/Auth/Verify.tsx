bea9729bb8b215ad623d3bcb0dd7ef97cc24c545
omerenma <godwin2341@gmail.com>
2023-07-14 15:49:42 +0100

    paystack verification

--- src/component/Auth/Verify.tsx
+++ src/component/Auth/Verify.tsx
@@ -5,7 +5,7 @@ import {useSearchParams} from 'next/navigation'
 const Verify = () => {
     const [params, setParams] = useState("")
     const [data, setData] = useState("")
-    const url = 'https://rymistc0jk.execute-api.us-east-1.amazonaws.com/dev/subscription/initialize'
+    const url = 'https://rymistc0jk.execute-api.us-east-1.amazonaws.com/dev/subscription/verify'
     const searchParams = useSearchParams()
     const reference = searchParams.get('reference')
    
