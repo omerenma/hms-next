f7bf715cbae15255facf89df419a3697797d1801
omerenma <godwin2341@gmail.com>
2023-08-02 22:01:37 +0100

    adding animation

--- src/component/Website/Description.tsx
+++ src/component/Website/Description.tsx
@@ -1,27 +1,54 @@
-import React from 'react'
+import React, { useRef, useEffect } from "react";
+import { motion, useInView } from "framer-motion";
 const Description = () => {
-   
-  return (
-   
-    <div className='home-description'>
-      <h1>Why HMS?</h1>
-      <div className='home-header-description'>
-        <div className='home-header-description-card'>
-             <span>hhhhhhhhh</span>  <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </div>
-        <div className='home-header-description-card'>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        </div>
-        <div className='home-header-description-card'>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        </div>
-        <div className='home-header-description-card'>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        </div>
+  const container = useRef(null);
+  const ref = useRef(null);
+  //    const isInView = useInView({ root: container})
 
+  //    useEffect(() => {
+  //     console.log('Element is in view: ', isInView)
+  //    }, [isInView])
+  return (
+    <div ref={container} className="home-description">
+      <motion.h1
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 0.5, delay: 0.25 }}
+      >
+        Why choose K-HMS?
+      </motion.h1>
+      <div className="home-header-description">
+        <div className="home-header-description-card">
+          <p className="home-header-description-card-p">Automation</p>
+          <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className="home-header-description-card">
+          <p className="home-header-description-card-p">Reliable</p>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className="home-header-description-card">
+          <p className="home-header-description-card-p">Staff record</p>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className="home-header-description-card">
+        <p className="home-header-description-card-p">Patients record</p>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className="home-header-description-card">
+        <p className="home-header-description-card-p">Data persistency</p>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className="home-header-description-card">
+        <p className="home-header-description-card-p">Efeciency</p>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Description
+export default Description;
