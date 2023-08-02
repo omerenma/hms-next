5a8ea03d5ba45ee43e268678ae12399c03535a8e
omerenma <godwin2341@gmail.com>
2023-08-02 18:59:35 +0100

    adding animation

--- src/component/Website/Home.tsx
+++ src/component/Website/Home.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useEffect, useState, useRef} from 'react'
+import {motion, useInView, useAnimation} from 'framer-motion'
 import Navbar from './Navbar/Navbar'
 import Header from './Header/Header'
 import Footer from './Footer/Footer'
@@ -6,10 +7,29 @@ import Description from './Description'
 import Subscription from './Subscription'
 
 const HomePage = () => {
+    const ref = useRef(null)
+    const isInView = useInView(ref)
+    const mainControls = useAnimation()
+
+    useEffect(() => {
+        if(isInView){
+            mainControls.start("visible")
+        }
+    }, [isInView])
   return (
-    <div style={{width:"100%"}}>
+    <div ref={ref} style={{width:"100%"}}>
         <Navbar/>
-        <Header />
+        <motion.div
+        variants={{
+            hidden:{opacity:0, y:75},
+            visible: {opacity:1, y:0}
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{duration:0.5, delay:0.25}}
+        >
+        <Header /> 
+        </motion.div>
     <div className='content-container'>
         <div style={{maxWidth:1200}}>
         <Description/>
