a990f214328aafdeac99628a2e164f09a99b010d
omerenma <godwin2341@gmail.com>
2023-08-03 08:35:59 +0100

    adding animation

--- src/component/Website/Home.tsx
+++ src/component/Website/Home.tsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState, useRef} from 'react'
+
 import {motion, useInView, useAnimation} from 'framer-motion'
 import Navbar from './Navbar/Navbar'
 import Header from './Header/Header'
@@ -7,29 +8,32 @@ import Description from './Description'
 import Subscription from './Subscription'
 
 const HomePage = () => {
-    const ref = useRef(null)
-    const isInView = useInView(ref)
-    const mainControls = useAnimation()
+   
+    // const ref = useRef(null)
+    // const isInView = useInView(ref)
+    // const mainControls = useAnimation()
 
-    useEffect(() => {
-        if(isInView){
-            mainControls.start("visible")
-        }
-    }, [isInView])
+    // useEffect(() => {
+    //     if(isInView){
+    //         mainControls.start("visible")
+    //     }
+    // }, [isInView])
+
   return (
-    <div ref={ref} style={{width:"100%"}}>
+    <div  style={{width:"100%"}}>
+       
         <Navbar/>
-        <motion.div
+        <Header /> 
+        {/* <motion.div
         variants={{
             hidden:{opacity:0, y:75},
             visible: {opacity:1, y:0}
         }}
         initial="hidden"
-        animate={mainControls}
+        // animate={mainControls}
         transition={{duration:0.5, delay:0.25}}
         >
-        <Header /> 
-        </motion.div>
+        </motion.div> */}
     <div className='content-container'>
         <div style={{maxWidth:1200}}>
         <Description/>
--- src/component/Website/Navbar/Navbar.tsx
+++ src/component/Website/Navbar/Navbar.tsx
@@ -1,21 +1,42 @@
-import React from "react";
-import { links } from "../Link";
+import React, { useState } from "react";
+import DehazeIcon from "@mui/icons-material/Dehaze";
+import CloseIcon from "@mui/icons-material/Close";
 import Link from "next/link";
-const Navbar = () => {
-  return (
-    <div className="container-nav" >
-    <div className="navigation" >
-      <div className="left-nav">
-        <Link href={"product"} className="nav-item">Product</Link>
-        <Link href={"product"} className="nav-item">Solution</Link>
-        <Link href={"product"} className="nav-item">Business</Link>
-      </div>
-      <div className="right-nav">
-        <Link href={"login"} className="nav-item">Log in</Link>
-        <Link href={"account"} className="nav-item signup">Sign up</Link>
-      </div>
-    </div>
 
+const Navbar = () => {
+  const [isOpen, setOpen] = useState(false);
+  const handleTogle = () => {
+    setOpen((prev) => !prev);
+  };
+
+  return (
+    <div>
+      <div className="hamburger" onClick={handleTogle}>
+        {!isOpen ? <DehazeIcon className="hamburger-icon" /> : <CloseIcon className="hamburger-icon"/>}
+      </div>
+      <div className={!isOpen ? "container-nav" : ''}>
+        <div className="navigation">
+          <div className="left-nav">
+            <Link href={"product"} className="nav-item">
+              Product
+            </Link>
+            <Link href={"product"} className="nav-item">
+              Solution
+            </Link>
+            <Link href={"product"} className="nav-item">
+              Business
+            </Link>
+          </div>
+          <div className="right-nav">
+            <Link href={"login"} className="nav-item">
+              Log in
+            </Link>
+            <Link href={"account"} className="nav-item signup">
+              Sign up
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
